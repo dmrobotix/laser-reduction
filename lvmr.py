dd9aaffd2534be19f821ebe3f804bc274e11c455
dmrobotix <polylegendre@gmail.com>
2015-07-16 13:44:22 -0700

    Testing pycharm.

---
+++ lvmr.py
@@ -0,0 +1,10 @@
+__author__ = "Margot Paez"
+
+class Reducer(object):
+    def __init__(self, begin, end):
+        self.__begin = begin
+        self.__end = end
+        self.__ingvolt = []
+        self.__ingphot = []
+        
+
