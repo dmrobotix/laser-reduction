121417c4c731a2a7d7239b531f1cbbd2518cef4b
dmrobotix <polylegendre@gmail.com>
2015-07-16 18:29:45 -0700

    Updating files.

--- lvmr.py
+++ lvmr.py
@@ -32,64 +32,78 @@ class Reducer(object):
         """ Pulls in data from the file and integrates the column data """
 
         # open the file
-        try:
-            fl = open(filename, 'r')
-        except PermissionError:
-            return "Not able to access the file."
-        else:
-            with fl:
-                # read each line
-                for line in fl:
-                    # look for the last line of the header
-                    if line == "***End_of_Header***":
-                        break
+        fl = open(filename, 'r')
+        for line in fl:
+            # look for the last line of the header
+            if line == "***End_of_Header***":
+                break
 
-                # skip the next line
-                fl.readline()
+        # skip the next line
+        fl.readline()
 
-                # continue reading the lines
-                for line in fl:
-                        # the rest of the lines in the file are columns for various parameters
-                        # recorded during measurement determine if the values are within the pulse range
+        # continue reading the lines
+        for line in fl:
+                # the rest of the lines in the file are columns for various parameters
+                # recorded during measurement determine if the values are within the pulse range
 
-                        self.__counter += 1
+                self.__counter += 1
 
-                        if self.__begin <= self.__pulse <= self.__end:
-                            # split the line
-                            values = line.split()
+                if self.__begin <= self.__pulse <= self.__end:
+                    # split the line
+                    values = line.split()
 
-                            # roh only wants them summed not truly integrated in the traditional sense
-                            self.__ingvolt += values[1]
-                            self.__ingphot += values[2]
-                            self.__ingcurr += values[3]
-                            self.__ingttl += values[4]
+                    # roh only wants them summed not truly integrated in the traditional sense
+                    self.__ingvolt += values[1]
+                    self.__ingphot += values[2]
+                    self.__ingcurr += values[3]
+                    self.__ingttl += values[4]
 
-                        elif self.__pulse < self.__begin:
-                            # don't do anything with the data, just keep count of the data/pulse
-                            self.__counter += 1
+                elif self.__pulse < self.__begin:
+                    # don't do anything with the data, just keep count of the data/pulse
+                    self.__counter += 1
 
-                        elif self.__pulse > self.__end:
-                            # complete integration and stop reading the file
-                            self.__ingvolt = np.array(self.__ingvolt)
-                            self.__ingphot = np.array(self.__ingphot)
-                            self.__ingcurr = np.array(self.__ingcurr)
-                            self.__ingttl = np.array(self.__ingttl)
+                elif self.__pulse > self.__end:
+                    # complete integration and stop reading the file
+                    self.__ingvolt = np.array(self.__ingvolt)
+                    self.__ingphot = np.array(self.__ingphot)
+                    self.__ingcurr = np.array(self.__ingcurr)
+                    self.__ingttl = np.array(self.__ingttl)
 
-                            # close the file and exit the function
-                            fl.close()
+                    # close the file and exit the function
+                    fl.close()
 
-                            return True
+                    return True
 
-                        if self.__counter == 200:
-                            self.__counter = 0
-                            self.__pulse += 1
+                if self.__counter == 200:
+                    self.__counter = 0
+                    self.__pulse += 1
 
-    def plotter(self, directory):
-        """ Takes the ingration data and plots them as histogram and line plots and saves them into
+    def plotter(self):
+        """ Takes the integration data and plots them as histogram and line plots and saves them into
          a user defined directory """
         # duration of each pulse is 200 microseconds
         duration = 200e-06
-        distance = (self.__end - self.__begin) * duration
-        t = np.linspace(0, distance, duration)
+        pulsedist = (self.__end - self.__begin)
+        t = []
 
+        for pulse in range(pulsedist+2):
+            t.append(pulse*duration)
 
+        results = {"Voltage": self.__ingvolt,
+                   "Photo Diode": self.__ingphot,
+                   "Current": self.__ingcurr,
+                   "TTL Output": self.__ingttl}
+
+        for parameter in results.keys():
+            py.hist(results[parameter], bins=165, log=True)
+            py.xlabel(parameter)
+            py.title("Integrated %s Histogram" % parameter)
+            py.savefig("results/%s/%s-hist.png" % (self.__directory, parameter))
+
+            py.plot(t, results[parameter])
+            py.xlabel("Time (microsecs)")
+            py.ylabel(parameter)
+            py.title("Line Plot for Integrated %s" % parameter)
+            py.savefig("results/%s/%s-line.png" % (self.__directory, parameter))
+
+        return True
