c94c3778d921e34dacc7e40165eca48e8078a53e
dmrobotix <polylegendre@gmail.com>
2015-07-16 19:25:45 -0700

    Updating files.

--- lvm-main.py
+++ lvm-main.py
@@ -1,12 +1,24 @@
 __author__ = 'margot paez'
 
 import lvmr
-import sys
+from os import listdir
+from os.path import isfile, join
+import argparse
 
-files = sys.argv[0]
-parameters = sys.argv[0].split()
 
-rd = lvmr.Reducer(10, 400)
+parser = argparse.ArgumentParser(prog='python3 lvm-main.py', description='Reduce laser data. Any questions? Ask Margot: margot.paez@jpl.nasa.gov or mpz@ucsc.edu!')
+parser.add_argument(type=int, dest='begin_pulse', help='the starting pulse number (count begins at 1)')
+parser.add_argument(type=int, dest='end_pulse', help='the last pulse to get data from')
+parser.add_argument(type=str, dest='location', help='the directory that contains the laser files, use full path!')
+
+results = parser.parse_args()
+begin = results.begin_pulse
+end = results.end_pulse
+mypath = results.location
+
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+rd = lvmr.Reducer(begin, end)
 
 for file in files:
     if rd.integrate(file) is True:
